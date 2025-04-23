0370a6b6b9230d12c98a46138bb84fc97d714f2e
katzman <steve.katzman@coinbase.com>
2025-04-23 14:16:57 -0700

    Refactor to use updated ENS L2ReverseRegistrar

--- src/L2/ReverseRegistrarShim.sol
+++ src/L2/ReverseRegistrarShim.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.23;
 
 import {IReverseRegistrar} from "./interface/IReverseRegistrar.sol";
-import {IL2ReverseResolver} from "./interface/IL2ReverseResolver.sol";
+import {IL2ReverseRegistrar} from "./interface/IL2ReverseRegistrar.sol";
 
 contract ReverseRegistrarShim {
     address public immutable reverseRegistrar;
-    address public immutable reverseResolver;
+    address public immutable l2ReverseRegistrar;
     address public immutable l2Resolver;
 
-    constructor(address reverseRegistrar_, address reverseResolver_, address l2Resolver_) {
+    constructor(address reverseRegistrar_, address l2ReverseRegistrar_, address l2Resolver_) {
         reverseRegistrar = reverseRegistrar_;
-        reverseResolver = reverseResolver_;
+        l2ReverseRegistrar = l2ReverseRegistrar_;
         l2Resolver = l2Resolver_;
     }
 
@@ -19,9 +19,11 @@ contract ReverseRegistrarShim {
         address addr,
         string calldata name,
         uint256 signatureExpiry,
+        uint256[] memory cointypes,
         bytes memory signature
     ) external returns (bytes32) {
-        IReverseRegistrar(reverseRegistrar).setNameForAddr(addr, msg.sender, l2Resolver, name);
-        return IL2ReverseResolver(reverseResolver).setNameForAddrWithSignature(addr, name, signatureExpiry, signature);
+        IL2ReverseRegistrar(l2ReverseRegistrar).setNameForAddrWithSignature(addr, signatureExpiry, name, cointypes, signature);
+        return  IReverseRegistrar(reverseRegistrar).setNameForAddr(addr, msg.sender, l2Resolver, name);
+
     }
 }
