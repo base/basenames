c23333663d38118c2334407219098190381b6ebe
katzman <steve.katzman@coinbase.com>
2025-07-23 13:33:40 -0700

    Lint

--- src/L2/UpgradeableRegistrarController.sol
+++ src/L2/UpgradeableRegistrarController.sol
@@ -660,7 +660,7 @@ contract UpgradeableRegistrarController is Ownable2StepUpgradeable {
     /// @notice Internal helper for setting reverse records.
     ///
     /// @dev First sets the reverse record according to the legacy path via ReverseRegistrar. Then
-    ///     if the data is populated, sets the reverse record according to the ENSIP-19 compliant path 
+    ///     if the data is populated, sets the reverse record according to the ENSIP-19 compliant path
     ///     by calling ENS's L2ReverseRegistrar contract.
     ///
     /// @param name The name that will be set as the primary for msg.sender.
@@ -676,8 +676,7 @@ contract UpgradeableRegistrarController is Ownable2StepUpgradeable {
         bytes calldata signature
     ) internal {
         _setLegacyReverseRecord(name, resolver, msg.sender);
-        /// Skip L2ReverseRegistrar primary name setting when requisite data is not populated. 
-        if(signatureExpiry != 0 && coinTypes.length > 0 && signature.length > 0) {
+        if (signatureExpiry != 0 && coinTypes.length > 0 && signature.length > 0) {
             IL2ReverseRegistrar(_getURCStorage().l2ReverseRegistrar).setNameForAddrWithSignature(
                 msg.sender, signatureExpiry, name, coinTypes, signature
             );
